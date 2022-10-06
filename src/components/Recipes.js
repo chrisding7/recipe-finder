5a9b75e8a84a660277b383e1d88f0fb0680297d8
Chris Ding <chrisding7@berkeley.edu>
2022-10-06 10:11:17 -0700

    fixed env var naming

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -14,7 +14,7 @@ const Recipes = () => {
 
   // GET recipe data
     useEffect(() => {
-    fetch(`/v2?type=public&q=${searchSubmit}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`)
+    fetch(`/v2?type=public&q=${searchSubmit}&app_id=${REACT_APP_API_ID}&app_key=${REACT_APP_API_KEY}`)
     .then(res => res.json())
     .then(data => {
       if (data.message === "Usage limits are exceeded") {
